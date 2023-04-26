fd16df0cdb88ebcb319d3a0229ae7b0b4a2d45e9
lonkaars <loek@pipeframe.xyz>
2023-04-26 12:33:29 +0200

    WIP build system (more errors gone?)

--- zumo/zumo.mk
+++ zumo/zumo.mk
@@ -15,47 +15,51 @@ CFLAGS += -I./lib/ArduinoCore-avr/libraries/EEPROM/src
 CFLAGS += -I./lib/ArduinoCore-avr/libraries/Wire/src
 CFLAGS += -I./lib/ArduinoCore-avr/variants/circuitplay32u4
 
-LIBS += lib/ArduinoCore-avr/libraries/HID/src/HID.cpp \
-				lib/ArduinoCore-avr/libraries/SoftwareSerial/src/SoftwareSerial.cpp \
-				lib/ArduinoCore-avr/libraries/SPI/src/SPI.cpp \
-				lib/ArduinoCore-avr/libraries/Wire/src/Wire.cpp \
-				lib/ArduinoCore-avr/cores/arduino/HardwareSerial1.cpp \
-				lib/ArduinoCore-avr/cores/arduino/HardwareSerial0.cpp \
-				lib/ArduinoCore-avr/cores/arduino/WString.cpp \
-				lib/ArduinoCore-avr/cores/arduino/PluggableUSB.cpp \
-				lib/ArduinoCore-avr/cores/arduino/WMath.cpp \
-				lib/ArduinoCore-avr/cores/arduino/IPAddress.cpp \
-				lib/ArduinoCore-avr/cores/arduino/abi.cpp \
-				lib/ArduinoCore-avr/cores/arduino/HardwareSerial.cpp \
-				lib/ArduinoCore-avr/cores/arduino/CDC.cpp \
-				lib/ArduinoCore-avr/cores/arduino/new.cpp \
-				lib/ArduinoCore-avr/cores/arduino/Stream.cpp \
-				lib/ArduinoCore-avr/cores/arduino/HardwareSerial2.cpp \
-				lib/ArduinoCore-avr/cores/arduino/Print.cpp \
-				lib/ArduinoCore-avr/cores/arduino/main.cpp \
-				lib/ArduinoCore-avr/cores/arduino/HardwareSerial3.cpp \
-				lib/pololu-buzzer-arduino/src/PololuBuzzer.cpp \
-				lib/pushbutton-arduino/Pushbutton.cpp \
-				lib/zumo-32u4-arduino-library/src/QTRSensors.cpp \
-				lib/zumo-32u4-arduino-library/src/Zumo32U4IRPulses.cpp \
-				lib/zumo-32u4-arduino-library/src/Zumo32U4Encoders.cpp \
-				lib/zumo-32u4-arduino-library/src/Zumo32U4IMU.cpp \
-				lib/zumo-32u4-arduino-library/src/Zumo32U4Motors.cpp \
-				lib/zumo-32u4-arduino-library/src/Zumo32U4ProximitySensors.cpp \
-				lib/usb-pause-arduino/USBPause.cpp \
-				lib/pololu-hd44780-arduino/PololuHD44780.cpp \
-				lib/fastgpio-arduino/FastGPIO.cpp \
-				lib/pololu-oled-arduino/src/font.cpp
-# lib/ArduinoCore-avr/cores/arduino/Tone.cpp
-# lib/ArduinoCore-avr/libraries/Wire/src/utility/twi.c
+CFLAGS += -L/usr/avr/lib/avr5/ -L/usr/lib/gcc/avr/12.2.0/avr5 -lgcc -lm -lc -latmega32u4
+LFLAGS += -L/usr/avr/lib/avr5/ -L/usr/lib/gcc/avr/12.2.0/avr5 -lgcc -lm -lc -latmega32u4
 
-LIBS += lib/ArduinoCore-avr/cores/arduino/WInterrupts.c \
-				lib/ArduinoCore-avr/cores/arduino/hooks.c \
-				lib/ArduinoCore-avr/cores/arduino/wiring_shift.c \
-				lib/ArduinoCore-avr/cores/arduino/wiring_digital.c \
-				lib/ArduinoCore-avr/cores/arduino/wiring_analog.c \
-				lib/ArduinoCore-avr/cores/arduino/wiring_pulse.c \
-				lib/ArduinoCore-avr/cores/arduino/wiring.c
+LIBS += lib/ArduinoCore-avr/cores/arduino/PluggableUSB.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/CDC.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/HardwareSerial.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/USBCore.cpp
+# LIBS += lib/ArduinoCore-avr/cores/arduino/HardwareSerial0.cpp
+# LIBS += lib/ArduinoCore-avr/cores/arduino/HardwareSerial1.cpp
+# LIBS += lib/ArduinoCore-avr/cores/arduino/HardwareSerial2.cpp
+# LIBS += lib/ArduinoCore-avr/cores/arduino/HardwareSerial3.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/IPAddress.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/Print.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/Stream.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/WMath.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/WString.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/abi.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/main.cpp
+LIBS += lib/ArduinoCore-avr/cores/arduino/new.cpp
+LIBS += lib/ArduinoCore-avr/libraries/HID/src/HID.cpp
+LIBS += lib/ArduinoCore-avr/libraries/SPI/src/SPI.cpp
+# LIBS += lib/ArduinoCore-avr/libraries/SoftwareSerial/src/SoftwareSerial.cpp
+LIBS += lib/ArduinoCore-avr/libraries/Wire/src/Wire.cpp
+LIBS += lib/pololu-buzzer-arduino/src/PololuBuzzer.cpp
+LIBS += lib/pushbutton-arduino/Pushbutton.cpp
+LIBS += lib/zumo-32u4-arduino-library/src/QTRSensors.cpp
+LIBS += lib/zumo-32u4-arduino-library/src/Zumo32U4IRPulses.cpp
+LIBS += lib/zumo-32u4-arduino-library/src/Zumo32U4Encoders.cpp
+LIBS += lib/zumo-32u4-arduino-library/src/Zumo32U4IMU.cpp
+LIBS += lib/zumo-32u4-arduino-library/src/Zumo32U4Motors.cpp
+LIBS += lib/zumo-32u4-arduino-library/src/Zumo32U4ProximitySensors.cpp
+LIBS += lib/usb-pause-arduino/USBPause.cpp
+LIBS += lib/pololu-hd44780-arduino/PololuHD44780.cpp
+LIBS += lib/fastgpio-arduino/FastGPIO.cpp
+LIBS += lib/pololu-oled-arduino/src/font.cpp
+# LIBS += lib/ArduinoCore-avr/cores/arduino/Tone.cpp
+# LIBS += lib/ArduinoCore-avr/libraries/Wire/src/utility/twi.c
+
+LIBS += lib/ArduinoCore-avr/cores/arduino/WInterrupts.c
+LIBS += lib/ArduinoCore-avr/cores/arduino/hooks.c
+LIBS += lib/ArduinoCore-avr/cores/arduino/wiring_shift.c
+LIBS += lib/ArduinoCore-avr/cores/arduino/wiring_digital.c
+LIBS += lib/ArduinoCore-avr/cores/arduino/wiring_analog.c
+# LIBS += lib/ArduinoCore-avr/cores/arduino/wiring_pulse.c
+LIBS += lib/ArduinoCore-avr/cores/arduino/wiring.c
 
 SRCS += $(LIBS)
 
