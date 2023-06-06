051f0284c9895282205632087a2de6d163073cee
NielsCoding <48092678+heavydemon21@users.noreply.github.com>
2023-06-06 20:30:44 +0200

    WIP final integration

--- nicla/road.py
+++ nicla/road.py
@@ -1,12 +1,13 @@
 import sensor, image, time, math
 import uart
+import signs_detect
 import traffic_light
 from consts import *
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.HVGA)
-sensor.skip_frames(time = 2000)
+sensor.skip_frames(time = 4000)
 clock = time.clock()
 
 WIDTH = 480
@@ -33,14 +34,12 @@ def drive(driveImg):
 
   offset_sum = 0.0
   offset_count = 0.0
-
   for blob in img.find_blobs([(ROAD_MIN_BRIGHTNESS, 0xff)], pixels_threshold=100):
     img.draw_rectangle(blob.rect())
     area_weight = blob.area()
     horizontal_pos = (blob.x() + blob.w()/2) / WIDTH
     offset_sum += horizontal_pos * area_weight
     offset_count += area_weight
-
   # dit tegen niemand zeggen
   if offset_count < 0.01: return
   avg = offset_sum / offset_count
@@ -52,13 +51,22 @@ def drive(driveImg):
   steerByte = int((avg + 1.0) * (DUI_CMD_STEER_END - DUI_CMD_STEER_START) / 2 + DUI_CMD_STEER_START)
   uart.uart_buffer(steerByte)
   sensor.dealloc_extra_fb()
-
+count = 0
 while(True):
-  img = sensor.snapshot()
-  data = traffic_light.traf_lights(img)
-  if data is not None:
-      print(data)
-      uart.uart_buffer(data)
-  drive(img)
-  #uart.uart_buffer(DUI_CMD_SPEED_END)
-  clock.tick()
+  if count == 0:
+    count = count + 1
+    speed = signs_detect.init_kpts("speed")
+    #stop = signs_detect.init_kpts("stop")
+    #car = signs_detect.init_kpts("image")
+  else:
+      img = sensor.snapshot()
+      data = traffic_light.traf_lights(img)
+      if data is not None:
+          uart.uart_buffer(data)
+
+        #data_sign = signs_detect.sign_detection(img)
+        #if data_sign is not None:
+          #uart.uart_buffer(data_sign)
+
+      drive(img)
+      #uart.uart_buffer(DUI_CMD_SPEED_END)
---
+++ nicla/signs_detect.py
@@ -0,0 +1,64 @@
+import sensor, image
+
+kpts_threshold = 20
+kpts_corner = image.CORNER_FAST
+
+def init_kpts(str):
+    temp_img = image.Image(f"./{str}.jpg",copy_to_fb=True)
+    temp_img.to_grayscale()
+    kpts = temp_img.find_keypoints(max_keypoints=128, threshold=kpts_threshold, corner_detector=kpts_corner, scale_factor=1.2)
+    temp_img.dealloc_extra_fb()
+    return kpts
+
+def match_kpts(kpts0, kpts1):
+    if kpts0 is not None and kpts1 is not None:
+        match = image.match_descriptor(kpts0, kpts1, threshold=70)
+        #print("matched:%d dt:%d"%(match.count(), match.theta()))
+        if match.count() > 0:
+            print(match.count())
+        return match.count() > 1
+    else:
+        return 0
+
+def read_red_sign(val, img, kpts):
+    if match_kpts(kpts, stop):
+        img.draw_rectangle(val.rect())
+        #img.draw_cross(match.cx(), match.cy(), size=10)
+        #print("stop")
+        return 0x01
+    if match_kpts(kpts, speed):
+        img.draw_rectangle(val.rect())
+        #print("speed")
+        return 0x02
+    if match_kpts(kpts, car):
+        img.draw_rectangle(val.rect())
+        #print("car")
+        return 0x03
+
+#def read_red_sign(val, img, kpts):
+
+def sign_detection(img_sign):
+    img = img_sign.copy()
+    ######## Detect signs
+    blobs_r = img.find_blobs([(0, 100, 25, 63, -128, 127)])
+    blobs_b = img.find_blobs([(0, 29, 11, -128, -31, -5)])
+    #print(f"old: { len(blobs_r) + len(blobs_b) }")
+
+    blobs_r[:] = [b for b in blobs_r if (b.convexity() < 0.7 and b.area() > 64)]
+    blobs_b[:] = [b for b in blobs_b if (b.convexity() < 0.7 and b.area() > 64)]
+    #print(f"new: { len(blobs_r) + len(blobs_b) }")
+
+
+    ######## Read signs
+    img = img.to_grayscale()
+    sign_buffer = 0x00
+    if(len(blobs_r) > 0 or len(blobs_b) > 0):
+        kpts_img = img.find_keypoints(max_keypoints=255, threshold=kpts_threshold, corner_detector=kpts_corner)
+
+        for index, b in enumerate(blobs_r):
+            sign_buffer = read_red_sign(b, img, kpts_img)
+
+        for index, b in enumerate(blobs_b):
+            sign_buffer = read_blu_sign(b, img, kpts_img)
+    sensor.dealloc_extra_fb()
+    return sign_buffer
